f8a1422fa52139f86c386f7d5ca5c5e19542ecb2
Patrick Asiedu <pattribette@gmail.com>
2022-05-26 13:00:58 +0000

    order cards

---
+++ src/components/Cards/FoodCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FoodCard = () => {
+  return (
+    <div className=" lg:w-[230px] 2xl:w-[320px] h-[140px] box-outer-shadow rounded-3xl flex flex-col pl-9  justify-evenly  ">
+      <h1 className=" ">Banku and Tilapia</h1>
+      <h1 className="text-2xl font-bold">14</h1>
+    </div>
+  );
+};
+
+export default FoodCard;
